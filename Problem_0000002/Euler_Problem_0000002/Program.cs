445c6f86fcdb18bfcf4415a654998c4f47adbc50
Digitalroot <digitalroot@gmail.com>
2013-08-28 23:31:03 -0700

    Solved Problem 2

---
+++ Problem_0000002/Euler_Problem_0000002/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Euler_Problem_0000002
+{
+  internal class Program
+  {
+    private static void Main()
+    {
+      var validIntList = new List<Int64>();
+      Int64 j = 0;
+      Int64 i = 1;
+
+      while (i < 4000000)
+      {
+        if (IsEven(i))
+        {
+          validIntList.Add(i);
+        }
+        Int64 g = i;
+        i = j + i;
+        j = g;
+      }
+
+      if (validIntList.Count > 0)
+      {
+        Int64 total = validIntList.Aggregate<long, long>(0, (current, l) => current + l);
+        Console.WriteLine(total);
+      }
+
+      Console.ReadLine();
+    }
+
+    private static bool IsEven(Int64 i)
+    {
+      return i%2 == 0;
+    }
+  }
+}
