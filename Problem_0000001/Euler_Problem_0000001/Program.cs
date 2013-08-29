a4e8494a15904984503dd5cb1f868b504439985d
Digitalroot <digitalroot@gmail.com>
2013-08-28 23:09:04 -0700

    Solved Problem 1

---
+++ Problem_0000001/Euler_Problem_0000001/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Euler_Problem_0000001
+{
+  internal class Program
+  {
+    private static void Main()
+    {
+      var validIntList = new List<int>();
+      for (var i = 1; i < 1000; i++)
+      {
+        if (IsMultipleOfThreeOrFive(i))
+        {
+          validIntList.Add(i);
+        }
+      }
+
+      if (validIntList.Count > 0)
+      {
+        var total = validIntList.Aggregate(0, (current, i) => current + i);
+        Console.WriteLine(total);
+      }
+
+      Console.ReadLine();
+    }
+
+    private static bool IsMultipleOfThreeOrFive(int i)
+    {
+      return i%3 == 0 || i%5 == 0;
+    }
+  }
+}
