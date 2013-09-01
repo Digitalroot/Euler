1037513b219f45bd6251c516b428ad32ad70a74c
Digitalroot <digitalroot@gmail.com>
2013-09-01 15:46:58 -0700

    Solved Problem 7

---
+++ Euler_Problem_0000007/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Euler_Problem_0000007
+{
+  internal class Program
+  {
+    private static void Main()
+    {
+      List<long> pList = new PrimeNumberUtil().GetPrimes(10001);
+      Console.WriteLine(pList[10000]);
+      Console.ReadKey();
+    }
+  }
+}
