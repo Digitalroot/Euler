8d4d7f0a45d9779f1fa489544f304154967f4ef1
Digitalroot <digitalroot@gmail.com>
2013-09-01 15:21:02 -0700

    Solved Problem 6

---
+++ Euler_Problem_0000006/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Euler_Problem_0000006
+{
+  internal class Program
+  {
+    private static void Main()
+    {
+      double sumOfSquares = SumSquares(1, 100);
+      double squareOfSums = SquareSums(1, 100);
+
+      double diff = squareOfSums - sumOfSquares;
+
+      Console.WriteLine(diff);
+      Console.ReadKey();
+    }
+
+    private static double SquareSums(int min, int max)
+    {
+      double sum = 0;
+      for (int i = min; i <= max; i++)
+      {
+        sum += i;
+      }
+      return Math.Pow(sum, 2);
+    }
+
+    private static double SumSquares(int min, int max)
+    {
+      double sum = 0;
+      for (int i = min; i <= max; i++)
+      {
+        sum = sum + Math.Pow(i, 2);
+      }
+      return sum;
+    }
+  }
+}
