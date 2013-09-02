32c347a333a98507f0a4781b7b86d5db6f2f4d45
Digitalroot <digitalroot@gmail.com>
2013-09-01 23:35:22 -0700

    Solved Problem 9

---
+++ Euler_Problem_0000009/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Euler_Problem_0000009
+{
+  internal class Program
+  {
+    private static void Main()
+    {
+      const int sum = 1000;
+      int a;
+      for (a = 1; a <= sum/3; a++)
+      {
+        int b;
+        for (b = a + 1; b <= sum/2; b++)
+        {
+          int c = sum - a - b;
+          if (c > 0 && (a*a + b*b == c*c))
+            Console.WriteLine("a={0}, b={1}, c={2}, prod={3}", a, b, c, a*b*c);
+        }
+      }
+      Console.ReadKey();
+    }
+  }
+}
